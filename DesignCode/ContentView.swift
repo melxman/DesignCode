feadadb47bd0d0dd9a94a2e5f62069e3f91ed0d0
melxman <mafucy@163.com>
2020-02-14 10:01:27 +0800

    20200214 动画过渡 animation transition

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -9,37 +9,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var show = false  //是否允许动画状态之间进行切换.要在body之前
+    
     var body: some View {
         ZStack {
             TitleView()       //命名要遵守规则，还要明确和突出作用，独一无二
-            .blur(radius: 20)     //模糊(半径)
+                .blur(radius: show ? 20 : 0)     //模糊(半径)
+                .animation(.default)  //default类似easeInOut的0.3到0.5
             
             BackCardView()                  //卡片顺序与显示顺序相反，第下面一个在最前面展示
-                .background(Color("card4"))
+                .background(show ? Color("card3") : Color("card4"))
                 .cornerRadius(20)          //圆角
                 .shadow(radius: 20)          //阴影
-                .offset(x: 0, y: -40)       //位移
+                .offset(x: 0, y: show ? -400 : -40)       //位移
                 .scaleEffect(0.9)           //比例
-                .rotationEffect(.degrees(10))  //旋转两种写法1,忽略类型本身
+                .rotationEffect(.degrees(show ? 0:10))  //旋转两种写法1,忽略类型本身
                 .rotation3DEffect(Angle(degrees: 10), axis: (x: 10.0, y: 0, z: 0))                  //3d旋转
                 .blendMode(.hardLight)      //混合模式
+                .animation(.easeInOut(duration: 0.5))  //0.5为了错开动画
             
             BackCardView()
-                .background(Color("card3"))
+                .background(show ? Color("card4") : Color("card3"))
                 .cornerRadius(20)
                 .shadow(radius: 20)
-                .offset(x: 0, y: -20)
+                .offset(x: 0, y: show ? -200 : -20)
                 .scaleEffect(0.95)
-                .rotationEffect(Angle.degrees(5))
+                .rotationEffect(Angle.degrees(show ? 0 : 5))  //三元运算符判断是否分别对应的值
                 //.rotationEffect(Angle(degrees:5))   //2,Angle角度
                 .rotation3DEffect(Angle(degrees: 5), axis: (x: 10.0, y: 0, z: 0))
                 .blendMode(.hardLight)   //强光
+            //.animation(.linear)    //线性动画对于不透明度的改变，旋转这些不需要动量的动画非常好
+                .animation(.easeInOut(duration: 0.3))  //动画时间建议不要超过1秒避免影响UI渲染
             
             CardView()      //修改组件,变量,数据时，会暂停预览,需要手动resume
                 .blendMode(.hardLight)
+            .onTapGesture {                 //顶端手势
+                self.show.toggle()      //当用户点击时，是否发生动作.play可以预览操作
+            }
             
             BottonCardView()
-            .blur(radius: 20)
+                .blur(radius:show ? 20 : 0)
+            .animation(.default)
         }
     }
 }
